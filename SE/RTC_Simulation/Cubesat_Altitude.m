ffdaaa268a9b0689e1173c8e4850fe58c01ea0b9
tomoshimada <tshimada@ucsc.edu>
2020-04-25 10:00:00 -0700

    Special/General Relativity rev 1.0 4/25/20

--- SE/RTC_Simulation/Cubesat_Altitude.m
+++ SE/RTC_Simulation/Cubesat_Altitude.m
@@ -11,7 +11,7 @@ altitudes = [100 200 300 400 500 600 700 800 900];
 T = 5400;
 t = 63072000;
 craft_altitude_period_n = randi([1 9], 1, t/T).*100;
-period_time = 0:5400:63072000;
+period_time = linspace(0,t/T, t/T);
 
 lifetime_altitude = zeros(1, t);
 ii = 1;
@@ -19,6 +19,10 @@ while ii < t/T
     lifetime_altitude(ii:ii+5399) = (craft_altitude_period_n(ii+1)-craft_altitude_period_n(ii))/(period_time(ii+1)-period_time(ii))+craft_altitude_period_n(ii);
     ii = ii + 1;
 end
+
+figure;
+plot(t, lifetime_altitude);
+ylim([50 1000]);
 
 
         
